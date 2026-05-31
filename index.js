8184482fba1ba7c451126cd606d1d60805b3ff91
Mustiizm38 <mustiizmsowp@gmail.com>
2026-05-31 22:33:40 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages
+  ]
+});
+
+client.once("ready", () => {
+  console.log(`Bot giriş yaptı: ${client.user.tag}`);
+});
+
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === "verify") {
+    await interaction.reply("Verify sistemi aktif!");
+  }
+});
+
+client.login(process.env.TOKEN);
